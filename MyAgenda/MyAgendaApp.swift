3c39b9858e7783b1bf888bc726711a20b27d2f9e
ssabrut <michael.gunawan2002@gmail.com>
2022-03-10 15:21:53 +0700

    Initial Commit

---
+++ MyAgenda/MyAgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAgendaApp.swift
+//  MyAgenda
+//
+//  Created by MacBook Pro on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
